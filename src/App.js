e35bb6056b43a5e18996c447a2f73e68fd4dd253
Nadezhda <catsnake@yandex.ru>
2021-09-14 15:04:00 +0300

    visual part of app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles/App.css";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [posts, setPosts] = useState([
+		{ id: 1, title: "JavaScript 1", body: "Decription" },
+		{ id: 2, title: "JavaScript 2", body: "Decription" },
+		{ id: 3, title: "JavaScript 3", body: "Decription" },
+	]);
+
+	return (
+		<div className="App">
+			<form>
+				<MyInput type="text" placeholder="Название поста" />
+				<MyInput type="text" placeholder="Описание поста" />
+				<MyButton disabled>Создать пост</MyButton>
+			</form>
+			<PostList posts={posts} title="Посты про JS" />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const PostItem = (props) => {
+	return (
+		<div>
+			<div className="post">
+				<div className="port_content">
+					<strong>
+						{props.post.id}. {props.post.title}
+					</strong>
+					<div>{props.post.body}</div>
+				</div>
+				<div className="port__btns">
+					<button>Удалить</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default PostItem;
