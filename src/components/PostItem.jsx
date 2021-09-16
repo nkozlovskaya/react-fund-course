6dc51a09c08c2651ae0db2bf47ad79c87db5669b
Nadezhda <catsnake@yandex.ru>
2021-09-16 16:45:40 +0300

    add page for each post and information about it

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import MyButton from "./UI/button/MyButton";
 
 const PostItem = (props) => {
+	const router = useHistory();
 	return (
 		<div>
 			<div className="post">
@@ -10,6 +12,11 @@ const PostItem = (props) => {
 						{props.post.id}. {props.post.title}
 					</strong>
 					<div>{props.post.body}</div>
+				</div>
+				<div className="port__btns">
+					<MyButton onClick={() => router.push(`/posts/${props.post.id}`)}>
+						Открыть
+					</MyButton>
 				</div>
 				<div className="port__btns">
 					<MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
