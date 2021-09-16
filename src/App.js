b233f7aab950c517cd5224b24bfc53ead78cd17e
Nadezhda <catsnake@yandex.ru>
2021-09-16 14:00:07 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -8,39 +8,48 @@ import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount } from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
 	const [posts, setPosts] = useState([]);
 	const [filter, setFilter] = useState({ sort: "", query: "" });
 	const [modal, setModal] = useState(false);
+	const [totalPages, setTotalPages] = useState(0);
+	const [limit, setLimit] = useState(10);
+	const [page, setPage] = useState(1);
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-	const [isPostLoading, setIsPostLoading] = useState(false);
+
+	const [fetchPosts, isPostLoading, postError] = useFetching(
+		async (limit, page) => {
+			const response = await PostService.getAll(limit, page);
+			setPosts(response.data);
+			const totalCount = response.headers["x-total-count"];
+			setTotalPages(getPageCount(totalCount, limit));
+		}
+	);
 
 	useEffect(() => {
-		fetchPosts();
-	}, []);
+		fetchPosts(limit, page);
+	}, [page]);
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
 		setModal(false);
 	};
 
-	async function fetchPosts() {
-		setIsPostLoading(true);
-		setTimeout(async () => {
-			const posts = await PostService.getAll();
-			setPosts(posts);
-			setIsPostLoading(false);
-		}, 1000);
-	}
-
 	const removePost = (post) => {
 		setPosts(posts.filter((p) => p.id !== post.id));
 	};
 
+	const changePage = (page) => {
+		setPage(page);
+		fetchPosts(limit, page);
+	};
+
 	return (
 		<div className="App">
-			<button onClick={fetchPosts}>GET POSTS</button>
 			<MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
 				Создать пост
 			</MyButton>
@@ -50,6 +59,8 @@ function App() {
 
 			<hr style={{ margin: "15px 0" }} />
 			<PostFilter filter={filter} setFilter={setFilter} />
+
+			{postError && <h1>Произошла ошибка ${postError}</h1>}
 
 			{isPostLoading ? (
 				<div
@@ -64,6 +75,7 @@ function App() {
 					title="Посты про JS"
 				/>
 			)}
+			<Pagination page={page} changePage={changePage} totalPages={totalPages} />
 		</div>
 	);
 }
